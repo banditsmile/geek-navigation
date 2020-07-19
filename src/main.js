b683d9741f4a138c19cef4f8cb3ae85022925a99
bandit <banditsmile@qq.com>
2020-07-19 12:20:00 +0800

    代码恢复

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
-<<<<<<< HEAD
-import dayjs from 'dayjs'
-=======
 import axios from 'axios'
->>>>>>> c2cb2ef4349e2d1d7ad37d9516f4edb4d874777c
 import App from './App.vue'
 import router from "./router"
 import Mock from "./mock"
@@ -18,11 +14,6 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-<<<<<<< HEAD
-Vue.prototype.$api= api
-Vue.prototype.$dayjs= dayjs
-=======
->>>>>>> c2cb2ef4349e2d1d7ad37d9516f4edb4d874777c
 Vue.prototype.$storage= Storage
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-<<<<<<< HEAD
-import Index from "@/pages/index/Index.vue"
-import Login from "@/pages/Login.vue"
-import Admin from "@/pages/admin/Index.vue"
-import List from "@/pages/admin/List.vue"
-import Category from "@/pages/admin/Category.vue"
-import Audit from "@/pages/admin/Audit.vue"
-=======
 import Index from "@/pages/Index.vue";
 
->>>>>>> c2cb2ef4349e2d1d7ad37d9516f4edb4d874777c
 
 Vue.use(VueRouter);
 
@@ -19,36 +10,8 @@ const routes = [
 	{
 		path: '/',
 		name: 'index',
-<<<<<<< HEAD
-		component: Index,
-		meta: {
-			keepAlive: true
-		}
-	},
-	{
-		path: '/login',
-		component: Login
-	},
-	{
-		path: '/admin',
-		component: Admin,
-		children: [
-			{
-				path: '/',
-				component: Audit
-			},
-			{
-				path: 'list',
-				component: List
-			},
-			{
-				path: 'category',
-				component: Category
-			}
-		]
-=======
+
 		component: Index
->>>>>>> c2cb2ef4349e2d1d7ad37d9516f4edb4d874777c
 	}
 ]
 
